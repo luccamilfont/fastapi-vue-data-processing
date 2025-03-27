ce97f0acd4462757ad0ed7418525583e18a248b3
Lucca Araújo <luccamilfont@gmail.com>
2025-03-27 20:57:10 -0300

    minor csv-standardization.py improvement

--- data-processing-scripts/csv-standardization.py
+++ data-processing-scripts/csv-standardization.py
@@ -9,7 +9,9 @@ for fileName in filesPath.iterdir():
     if fileName.suffix == ".zip":
         with zipfile.ZipFile(fileName, 'r') as zipf:
             zipf.extractall(filesPath)
-            df = pd.read_csv(filesPath/fileName.with_suffix('.csv'), delimiter=';', parse_dates=['DATA'], dayfirst=True)
+            df = pd.read_csv(filesPath/fileName.with_suffix('.csv'), delimiter=';')
+            if 'DATA' in df.columns:
+                df['DATA'] = pd.to_datetime(df['DATA'], dayfirst=True)
             df['VL_SALDO_INICIAL'] = df['VL_SALDO_INICIAL'].str.replace(',', '.').astype(float)
             df['VL_SALDO_FINAL'] = df['VL_SALDO_FINAL'].str.replace(',', '.').astype(float)
             df.to_csv(filesPath/fileName.with_suffix('.csv'), sep=';', index=False)

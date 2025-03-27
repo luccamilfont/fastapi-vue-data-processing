1b318b6fae3a530e1057043a3635c4b5df4a702e
Lucca Araújo <luccamilfont@gmail.com>
2025-03-27 15:02:15 -0300

    postgresql integration for AsnDado POST request

--- api/main.py
+++ api/main.py
@@ -1,78 +1,104 @@
 from typing import Annotated
-
-from fastapi import Depends, FastAPI, HTTPException, Query
-from sqlmodel import Field, Session, SQLModel, create_engine, select
+from fastapi import Depends, FastAPI, HTTPException, Depends
+from pydantic import BaseModel
+import models
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
+from typing import List, Annotated
 from datetime import date
 from decimal import Decimal
 
+# sqlite_file_name = "database.db"
+# sqlite_url = f"sqlite:///{sqlite_file_name}"
 
-class Ans_dado(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    data: date
-    reg_ans: int
-    cd_conta_contabil: str = Field(max_length=20)
-    descricao: str = Field(max_length=255)
-    vl_saldo_inicial: Decimal = Field(ge=0)
-    vl_saldo_final: Decimal = Field(ge=0)
+# connect_args = {"check_same_thread": False}
+# engine = create_engine(sqlite_url, connect_args=connect_args)
 
+# def create_db_and_tables():
+#     SQLModel.metadata.create_all(engine)
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
+# def get_session():
+#     with Session(engine) as session:
+#         yield session
 
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, connect_args=connect_args)
-
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
-
-def get_session():
-    with Session(engine) as session:
-        yield session
-
-
-SessionDep = Annotated[Session, Depends(get_session)]
+# SessionDep = Annotated[Session, Depends(get_session)]
 
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+class AsnDadoBase(BaseModel):
+    data: date
+    reg_ans: int
+    cd_conta_contabil: str
+    descricao: str
+    vl_saldo_inicial: Decimal
+    vl_saldo_final: Decimal
+
+def get_db():
+    db= SessionLocal()
+    try: 
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+@app.post("/asnDado/")
+def create_asn_dado(asn_dado: AsnDadoBase, db: Session = Depends(get_db)):
+    db_asn_dado = models.AsnDado(
+        data=asn_dado.data,
+        reg_ans=asn_dado.reg_ans,
+        cd_conta_contabil=asn_dado.cd_conta_contabil,
+        descricao=asn_dado.descricao,
+        vl_saldo_inicial=asn_dado.vl_saldo_inicial,
+        vl_saldo_final=asn_dado.vl_saldo_final,
+    )
+
+    try:
+        db.add(db_asn_dado)
+        db.commit()
+        db.refresh(db_asn_dado) 
+
+        return {"message": "AsnDado created successfully", "id": db_asn_dado.asn_dado_id}
+
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=f"Error creating AsnDado: {e}")
+
+# @app.on_event("startup")
+# def on_startup():
+#     # create_db_and_tables()
+
+# @app.post("/ans_dados/")
+# def create_ans_dado(ans_dado: Ans_dado, session: SessionDep) -> Ans_dado:
+#     session.add(ans_dado)
+#     session.commit()
+#     session.refresh(ans_dado)
+#     return ans_dado
+
+# @app.get("/ans_dados/")
+# def read_ans_dados(
+#     session: SessionLocal,
+#     offset: int = 0,
+#     limit: Annotated[int, Query(le=100)] = 100,
+# ) -> list[Ans_dado]:
+#     ans_dados = session.exec(select(Ans_dado).offset(offset).limit(limit)).all()
+#     return ans_dados
 
 
-@app.on_event("startup")
-def on_startup():
-    create_db_and_tables()
+# @app.get("/ans_dados/{ans_dado_id}")
+# def read_ans_dado(ans_dado_id: int, session: SessionDep) -> Ans_dado:
+#     ans_dado = session.get(Ans_dado, ans_dado_id)
+#     if not ans_dado:
+#         raise HTTPException(status_code=404, detail="Ans_dado not found")
+#     return ans_dado
 
 
-@app.post("/ans_dados/")
-def create_ans_dado(ans_dado: Ans_dado, session: SessionDep) -> Ans_dado:
-    session.add(ans_dado)
-    session.commit()
-    session.refresh(ans_dado)
-    return ans_dado
-
-
-@app.get("/ans_dados/")
-def read_ans_dados(
-    session: SessionDep,
-    offset: int = 0,
-    limit: Annotated[int, Query(le=100)] = 100,
-) -> list[Ans_dado]:
-    ans_dados = session.exec(select(Ans_dado).offset(offset).limit(limit)).all()
-    return ans_dados
-
-
-@app.get("/ans_dados/{ans_dado_id}")
-def read_ans_dado(ans_dado_id: int, session: SessionDep) -> Ans_dado:
-    ans_dado = session.get(Ans_dado, ans_dado_id)
-    if not ans_dado:
-        raise HTTPException(status_code=404, detail="Ans_dado not found")
-    return ans_dado
-
-
-@app.delete("/ans_dados/{ans_dado_id}")
-def delete_ans_dado(ans_dado_id: int, session: SessionDep):
-    ans_dado = session.get(Ans_dado, ans_dado_id)
-    if not ans_dado:
-        raise HTTPException(status_code=404, detail="Ans_dado not found")
-    session.delete(ans_dado)
-    session.commit()
-    return {"ok": True}
+# @app.delete("/ans_dados/{ans_dado_id}")
+# def delete_ans_dado(ans_dado_id: int, session: SessionDep):
+#     ans_dado = session.get(Ans_dado, ans_dado_id)
+#     if not ans_dado:
+#         raise HTTPException(status_code=404, detail="Ans_dado not found")
+#     session.delete(ans_dado)
+#     session.commit()
+#     return {"ok": True}
---
+++ api/models.py
@@ -0,0 +1,35 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Date, Numeric
+from database import Base
+from sqlmodel import Field, Session, SQLModel, create_engine, select, Field
+from datetime import date
+from decimal import Decimal
+import sqlalchemy as sa
+
+# class Ans_dado(SQLModel, table=True):
+#     id: int | None = Field(default=None, primary_key=True)
+#     data: date
+#     reg_ans: int
+#     cd_conta_contabil: str = Field(max_length=20)
+#     descricao: str = Field(max_length=255)
+#     vl_saldo_inicial: Decimal = Field(ge=2)
+#     vl_saldo_final: Decimal = Field(ge=2)
+
+# class Testes(SQLModel, table=True):
+#     __tablename__ = 'teste_table'
+
+#     id: int = Field(default=None, primary_key=True, index=True)
+#     descricao: str = Field(sa_column=sa.Column(sa.String, index=True))
+
+#     class Config:
+#         arbitrary_types_allowed = True
+
+class AsnDado(Base):
+    __tablename__ = 'ans_dado'
+
+    asn_dado_id = Column(Integer, primary_key=True, autoincrement=True)
+    data = Column(Date)
+    reg_ans = Column(Integer)
+    cd_conta_contabil = Column(String(20))
+    descricao = Column(String(255))
+    vl_saldo_inicial = Column(Numeric(15, 2))
+    vl_saldo_final = Column(Numeric(15, 2))

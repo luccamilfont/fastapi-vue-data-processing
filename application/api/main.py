f3bdd1bb775dd19477c52116e04d10667896b036
Lucca Araújo <luccamilfont@gmail.com>
2025-03-27 19:28:34 -0300

    "operadora" table creation
    create_operadora POST request
    "asn_dado" table renamed to "transacao"

--- application/api/main.py
+++ application/api/main.py
@@ -11,13 +11,35 @@ from decimal import Decimal
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
-class AsnDadoBase(BaseModel):
+class TransacaoBase(BaseModel):
     data: date
     reg_ans: int
     cd_conta_contabil: int
     descricao: str
     vl_saldo_inicial: Decimal
     vl_saldo_final: Decimal
+
+class OperadoraBase(BaseModel):
+    reg_ans: int
+    cnpj: str
+    razao_social: str
+    nome_fantasia: str
+    modalidade: str
+    logradouro: str
+    numero: str
+    complemento: str
+    bairro: str
+    cidade: str
+    uf: str
+    cep: int
+    ddd: int
+    telefone: str
+    fax: str
+    endereco_eletronico: str
+    representante: str
+    cargo_representante: str
+    regiao_de_comercializacao: int
+    data_registro_ans: date
 
 def get_db():
     db= SessionLocal()
@@ -28,24 +50,59 @@ def get_db():
 
 db_dependency = Annotated[Session, Depends(get_db)]
 
-@app.post("/asnDado/")
-def create_asn_dado(asn_dado: AsnDadoBase, db: Session = Depends(get_db)):
-    db_asn_dado = models.AsnDado(
-        data=asn_dado.data,
-        reg_ans=asn_dado.reg_ans,
-        cd_conta_contabil=asn_dado.cd_conta_contabil,
-        descricao=asn_dado.descricao,
-        vl_saldo_inicial=asn_dado.vl_saldo_inicial,
-        vl_saldo_final=asn_dado.vl_saldo_final,
+@app.post("/operadora/")
+def create_operadora(operadora: OperadoraBase, db: Session = Depends(get_db)):
+    db_operadora = models.Operadora(
+        reg_ans=operadora.reg_ans,
+        cnpj=operadora.cnpj,
+        razao_social=operadora.razao_social,
+        nome_fantasia=operadora.nome_fantasia,
+        modalidade=operadora.modalidade,
+        numero=operadora.numero,
+        complemento=operadora.complemento,
+        bairro=operadora.bairro,
+        cidade=operadora.cidade,
+        uf=operadora.uf,
+        cep=operadora.cep,
+        ddd=operadora.ddd,
+        telefone=operadora.telefone,
+        fax=operadora.fax,
+        endereco_eletronico=operadora.endereco_eletronico,
+        representante=operadora.representante,
+        cargo_representante=operadora.cargo_representante,
+        regiao_de_comercializacao=operadora.regiao_de_comercializacao,
+        data_registro_ans=operadora.data_registro_ans,
     )
 
     try:
-        db.add(db_asn_dado)
+        db.add(db_operadora)
         db.commit()
-        db.refresh(db_asn_dado) 
+        db.refresh(db_operadora) 
 
-        return {"message": "AsnDado created successfully", "id": db_asn_dado.id}
+        return {"message": "'Operadora' created successfully", "id": db_operadora.id}
 
     except Exception as e:
         db.rollback()
-        raise HTTPException(status_code=400, detail=f"Error creating AsnDado: {e}")
+        raise HTTPException(status_code=400, detail=f"Error creating 'Operadora': {e}")
+    
+@app.post("/transacao/")
+def create_transacao(transacao: TransacaoBase, db: Session = Depends(get_db)):
+    db_transacao = models.Transacao(
+        data=transacao.data,
+        reg_ans=transacao.reg_ans,
+        cd_conta_contabil=transacao.cd_conta_contabil,
+        descricao=transacao.descricao,
+        vl_saldo_inicial=transacao.vl_saldo_inicial,
+        vl_saldo_final=transacao.vl_saldo_final,
+    )
+
+    try:
+        db.add(db_transacao)
+        db.commit()
+        db.refresh(db_transacao) 
+
+        return {"message": "'Transação' created successfully", "id": db_transacao.id}
+
+    except Exception as e:
+        db.rollback()
+        raise HTTPException(status_code=400, detail=f"Error creating 'Transação': {e}")
--- application/api/models.py
+++ application/api/models.py
@@ -5,13 +5,38 @@ from datetime import date
 from decimal import Decimal
 import sqlalchemy as sa
 
-class AsnDado(Base):
-    __tablename__ = 'ans_dado'
+class Transacao(Base):
+    __tablename__ = 'transacao'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     data = Column(Date)
-    reg_ans = Column(Integer)
+    reg_ans = Column(Integer, ForeignKey('operadora.reg_ans'))
     cd_conta_contabil = Column(Integer)
     descricao = Column(String(255))
     vl_saldo_inicial = Column(Numeric(15, 2))
     vl_saldo_final = Column(Numeric(15, 2))
+
+class Operadora(Base):
+    __tablename__ = 'operadora'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    reg_ans = Column(Integer, unique=True)
+    cnpj = Column(String(15))
+    razao_social = Column(String(255))
+    nome_fantasia = Column(String(255))
+    modalidade = Column(String(255))
+    logradouro = Column(String(255))
+    numero = Column(String(255))
+    complemento = Column(String(255))
+    bairro = Column(String(255))
+    cidade = Column(String(255))
+    uf = Column(String(255))
+    cep = Column(Integer)
+    ddd = Column(Integer)
+    telefone = Column(String(255))
+    fax = Column(String(255))
+    endereco_eletronico = Column(String(255))
+    representante = Column(String(255))
+    cargo_representante = Column(String(255))
+    regiao_de_comercializacao = Column(Integer)
+    data_registro_ans = Column(Date)
